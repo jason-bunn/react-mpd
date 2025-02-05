f309d3c5ac4df0646aad9d3bc70a680bd0ff187a
Jason Bunn <jason.bunn009@gmail.com>
2025-02-05 09:54:50 -0600

    mapping items to list for render

--- src/components/FetchTest.tsx
+++ src/components/FetchTest.tsx
@@ -2,17 +2,20 @@ import { useState, useEffect } from 'react'
 function FetchTest() {
 
     const [records, setRecords] = useState([]);
-
+    let recordItems: any[] = [];
     useEffect(() => {
         fetch('https://dev-j1s8np4kcjf3ota.api.raw-labs.com/mock/product/sales')
             .then(response => response.json())
-            .then(res => setRecords(res.slice(0, 10)))
+            .then(res => {
+                setRecords(res.slice(0, 10));
+                recordItems = records.map(record => <li>{record}</li>)
+            })
             .catch(err => console.log(err))
     },[])
 
     return (
         <>
-            <p>{records}</p>
+            <p>{recordItems}</p>
         </>
     )
 }
