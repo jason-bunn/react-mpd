6c4c013d17f732f64229440717ff4c350ff07fcf
jason-bunn <jason.bunn009@gmail.com>
2025-02-17 00:13:25 -0600

    changed fetch request to react-query

--- src/components/FetchTest.tsx
+++ src/components/FetchTest.tsx
@@ -1,24 +1,34 @@
-import { useState, useEffect } from 'react'
+
+import { useQuery } from "@tanstack/react-query"
+
+
+type Record = {
+    productname: string;
+    totalsales: string;
+  };
+ 
+  async function fetchPosts(): Promise<Record[]> {
+    const response = await fetch('https://dev-j1s8np4kcjf3ota.api.raw-labs.com/mock/product/sales');
+    if (!response.ok) {
+      throw new Error('Problem fetching posts');
+    }
+    const posts: Record[] = await response.json();
+    return posts;
+  }
+
+
 function FetchTest() {
 
-    const [records, setRecords] = useState<any[]>([]);
-    const [dataLoading, setLoading] = useState(true);
+    const { data, error, isLoading } = useQuery<Record[], Error>({queryKey: ["records"], queryFn: fetchPosts});
 
-
-    useEffect(() => {
-        fetch('https://dev-j1s8np4kcjf3ota.api.raw-labs.com/mock/product/sales')
-            .then(response => response.json())
-            .then(res => {
-                setRecords(res);
-                setLoading(false);
-            })
-            .catch(err => console.log(err))
-    },[])
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
         <>
-            <ul>{dataLoading ? <li>loading...</li> : (
-                records.map((record) => (<li key={record.productname}>{record.productname} {record.totalsales}</li>)
+            <ul>{isLoading ? <li>loading...</li> : (
+                data?.map((record) => (<li key={record.productname}>{record.productname} {record.totalsales}</li>)
                 ))}</ul>
         </>
     )
