7a88fd71a06b334c1dd52454df11a78de213943f
jason-bunn <jason.bunn009@gmail.com>
2025-02-19 14:36:05 -0600

    navbar properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,52 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import {
-    MDBCard,
-    MDBCardBody,
-    MDBContainer,
-} from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBContainer } from "mdb-react-ui-kit";
 
-import {
-    QueryClient,
-    QueryClientProvider
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './App.css'
+import "./App.css";
 
 import LoginForm from "./components/LoginForm";
 import HelloWorld from "./components/HelloWorld";
 import FetchTest from "./components/FetchTest";
 import NavBar from "./components/NavBar";
-
+import imagePath from "./assets/react.svg";
 // Create a queryclient
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState<string>();
 
-    const [loggedIn, setLoggedIn] = useState<string>();
-   
-    function onLoginClick(someArg: string) {
-        setLoggedIn(someArg);
-    }
+  function onLoginClick(someArg: string) {
+    setLoggedIn(someArg);
+  }
 
-    if (!loggedIn) {
-        return (
-            <>
-                <h1>Basic Test Page now with CI amazingness</h1>
-                <hr/>
-                <NavBar/>
-                <MDBContainer className="d-flex justify-content-center">
-                    <MDBCard className="w-50">
-                        <MDBCardBody>
-                            <LoginForm onClick={onLoginClick}>
-                                Form
-                            </LoginForm>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBContainer>
-                
-
-               
-            </>
-        )
-    }
+  if (!loggedIn) {
     return (
-        <>
-            
-            <h1>Welcome</h1>
+      <>
+        <h1>Basic Test Page now with CI amazingness</h1>
+        <hr />
+        <NavBar brandName="Bunn Co" imageSrc={imagePath} />
+        <MDBContainer className="d-flex justify-content-center">
+          <MDBCard className="w-50">
+            <MDBCardBody>
+              <LoginForm onClick={onLoginClick}>Form</LoginForm>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBContainer>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>Welcome</h1>
 
-            <HelloWorld currentUser={loggedIn} />
-            <QueryClientProvider client={queryClient}>
-                <FetchTest></FetchTest>
-            </QueryClientProvider>
-        </>
-    )
+      <HelloWorld currentUser={loggedIn} />
+      <QueryClientProvider client={queryClient}>
+        <FetchTest></FetchTest>
+      </QueryClientProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,23 @@
-
-
-function NavBar() {
-  return (
-    <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-            <img src="/public/vite.svg" width="30" height="30" className="d-inline-block align-top" alt=""/>
-            Bootstrap
-        </a>
-    </nav>
-  )
+interface NavBarProps {
+  brandName: string;
+  imageSrc: string;
 }
 
-export default NavBar
+function NavBar({ brandName, imageSrc }: NavBarProps) {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        <img
+          src={imageSrc}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+        {brandName}
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;
