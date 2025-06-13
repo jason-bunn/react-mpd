6a824ef56290c62f7f189db47611457be420b9e6
Jason Bunn <jason.bunn009@gmail.com>
2025-06-13 15:04:18 -0500

    Project page updates

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,7 +9,7 @@ const Projects: React.FC = () => (
         <div className='card'>
           <div className='card-body'>
             <h5 className='card-title'>Adventure Science Center VR</h5>
-            <p className='card-text'>Short description of the project.</p>
+            <p className='card-text'> Nashville’s first, large-scale, public, virtual reality installation with custom-designed experiences.</p>
             <a href='#' className='btn btn-primary'>View</a>
           </div>
         </div>
@@ -18,7 +18,8 @@ const Projects: React.FC = () => (
         <div className='card'>
           <div className='card-body'>
             <h5 className='card-title'>Franke VR Tradeshow booth</h5>
-            <p className='card-text'>Another cool project description.</p>
+            <p className='card-text'>Inside the headset, trade show attendees were met by Franke’s virtual trade show
+              guide.</p>
             <a href='#' className='btn btn-primary'>View</a>
           </div>
         </div>
@@ -26,8 +27,8 @@ const Projects: React.FC = () => (
       <div className='col-md-4'>
         <div className='card'>
           <div className='card-body'>
-            <h5 className='card-title'>Virtufab Prototype Visualizer</h5>
-            <p className='card-text'>Another cool project description.</p>
+            <h5 className='card-title'>Boo Bunny Plague</h5>
+            <p className='card-text'>Boo Bunny Plague is a comedy action adventure musical game taking place across multiple dimensions and featuring a storyline with musical numbers.</p>
             <a href='#' className='btn btn-primary'>View</a>
           </div>
         </div>
@@ -36,7 +37,7 @@ const Projects: React.FC = () => (
         <div className='card'>
           <div className='card-body'>
             <h5 className='card-title'>Aimlabs</h5>
-            <p className='card-text'>Another cool project description.</p>
+            <p className='card-text'>Aimlabs is the leading AI-powered aim trainer offering personalized tasks, real-time analytics, and game-specific tools to elevate your gameplay.</p>
             <a href='#' className='btn btn-primary'>View</a>
           </div>
         </div>
@@ -45,7 +46,7 @@ const Projects: React.FC = () => (
         <div className='card'>
           <div className='card-body'>
             <h5 className='card-title'>Aegis Descent</h5>
-            <p className='card-text'>Another cool project description.</p>
+            <p className='card-text'>Aegis Descent is a fast-paced action game set at the end of WW2</p>
             <a href='#' className='btn btn-primary'>View</a>
           </div>
         </div>
@@ -54,7 +55,7 @@ const Projects: React.FC = () => (
         <div className='card'>
           <div className='card-body'>
             <h5 className='card-title'>Walmart Universe of Play</h5>
-            <p className='card-text'>Another cool project description.</p>
+            <p className='card-text'> The ultimate virtual toy destination in Roblox.</p>
             <a href='#' className='btn btn-primary'>View</a>
           </div>
         </div>
